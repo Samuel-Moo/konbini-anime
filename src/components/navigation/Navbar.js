a2864a392306735019f22ed5cbf5a179b26bd6a4
DerekMZ <richyrichon0@gmail.com>
2023-02-03 13:11:09 -0700

    Favorited buttons

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -9,22 +9,26 @@ function Navbar({children}){
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
       <div className="drawer-content flex flex-col">
           {/* Navbar */}
-          <div className="sticky top-0 w-full navbar bg-base-300">
+          <div className="sticky top-0 w-full navbar bg-base-300 ">
               <div className="flex-none lg:hidden">
                   <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                   </label>
               </div> 
           <div className="flex-1 px-2 mx-2">
-            <button>Konbini Anime</button>
+            <a href="/">
+                <button>Konbini Anime</button>
+            </a>    
           </div>
           <div className="flex-none hidden lg:block">
               <ul className="menu menu-horizontal">
            {/* Navbar menu content here */}
-                  <li><a>Anime</a></li>
-                  <li><a>Manga</a></li>
-                  <li><a>About</a></li>
-                  <li><a>Contact</a></li>
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/Anime">Anime</a></li>
+                  <li><a href="/Manga">Manga</a></li>
+                  <li><a href="/Favorited">Favorited</a></li>
+                  <li><a href="/Log in">Log in</a></li>
+                  
 
               </ul>
           </div>
@@ -32,7 +36,7 @@ function Navbar({children}){
             <input type="text" placeholder="Search" className="input input-bordered" />
           </div>
           </div>
-  {/* Page content here */}
+  
       {children}{/*no le metan contenido aqui */}
       
       </div> 
@@ -40,10 +44,12 @@ function Navbar({children}){
           <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
           <ul className="menu p-4 w-80 bg-base-100">
       {/* Sidebar content here */}
-              <li><a>Anime</a></li>
-              <li><a>Manga</a></li>
-              <li><a>About</a></li>
-              <li><a>Contact</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/Anime">Anime</a></li>
+                <li><a href="/Manga">Manga</a></li>
+                <li><a href="/Favorited">Favorited</a></li>
+                <li><a href="/Log in">Log in</a></li>
+              
 
           </ul>
 
--- src/containers/pages/Favorited.jsx
+++ src/containers/pages/Favorited.jsx
@@ -1,17 +1,15 @@
 import Footer from "components/navigation/Footer"
-import Video from "components/content/Video"
+import Favorites from "components/Favorited/Favorites"
 import Navbar from "components/navigation/Navbar"
 import Layout from "hocs/layouts/layout"
 
-
-function Home(){
+function Favorited(){
     return(
         <Layout data-theme="mosquera">
                 <Navbar>
-                
-                    <Video/>
-                    Konnichiwa
-                    <button className="invisible btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-primary "> Nuevo Descubrimiento </button>
+                    
+                    <Favorites/>
+                    
                     
                
                 {/* <Footer/> */}
@@ -20,4 +18,4 @@ function Home(){
     )
 }
 
-export default Home 
+export default Favorited 
