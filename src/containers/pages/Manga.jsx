d08b11597fe3c6086fc0eee0a3a264ed35737dd9
HugoAlvidrez <hugoalvidrez309@gmail.com>
2023-02-10 10:53:02 -0700

    manga demo 10/02/2023

--- src/containers/pages/Manga.jsx
+++ src/containers/pages/Manga.jsx
@@ -1,14 +1,20 @@
 import Footer from "components/navigation/Footer"
 import Navbar from "components/navigation/Navbar"
 import Layout from "hocs/layouts/layout"
+import loveiswar from "assets/images/loveiswar.jpg"
+import drstone from "assets/images/drstone.jpg"
+import oshi from "assets/images/oshi.jpg"
+
 
 function Manga(){
     return(
         <Layout>
             <Navbar>
+                
+                <div>Manga</div>
             <div class="grid grid-cols-3 gap-4">
-                <div className="card w-96 bg-base-100 shadow-xl" top>
-                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="manga" /></figure>
+                <div className="card-compact w-96 bg-base-100 shadow-xl" top>
+                    <figure><img src= {loveiswar} alt="manga" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">
                             Love is war 
@@ -16,14 +22,15 @@ function Manga(){
                         </h2>
                         <p>Manga demo</p>
                         <div className="card-actions justify-end">
+                            back
                             <div className="badge badge-outline">Manga</div> 
                             <div className="badge badge-outline">Fresh</div>
                         </div>
                     </div>
                 </div>
 
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="manga" /></figure>
+                <div className="card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src={drstone} alt="manga" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">
                         dr stone
@@ -37,8 +44,8 @@ function Manga(){
                     </div>
                 </div>
 
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="manga" /></figure>
+                <div className="card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src={oshi} alt="manga" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">
                         oshi no ko
